7f5f5a6d29e28892288cc1982e07fc3885aefbf4
Marcin Moskal <mmos@memleak.pl>
2017-12-10 22:51:08 +0100

    Add content-providers exercise

--- exercises/02_content-providers/app/src/main/java/pl/edu/agh/kis/content_providers/MainActivity.java
+++ exercises/02_content-providers/app/src/main/java/pl/edu/agh/kis/content_providers/MainActivity.java
@@ -1,13 +1,94 @@
 package pl.edu.agh.kis.content_providers;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    Cursor contactsCursor;
+
+    void loadContacts() {
+        setContentView(R.layout.activity_main);
+
+        /*
+         * TODO #1 create projection with columns:
+         * - ContactsContract.Contacts.DISPLAY_NAME
+         * - ContactsContract.CommonDataKinds.Phone.NUMBER
+         * - ContactsContract.Contacts._ID
+         */
+        String contactProjection[] = {
+                //...
+        };
+
+        /*
+         * TODO #2 run query returning cursor for given projection using URI:
+         * ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+         */
+        //contactsCursor = ...;
+
+        // NO CHANGES NEEDED BEYOND THIS POINT
+        int to[] = {
+                R.id.name,
+                R.id.phone
+        };
+
+        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this,
+                R.layout.contact_item,
+                contactsCursor,
+                contactProjection,
+                to,
+                0);
+
+        ListView contactList = findViewById(R.id.contacts_listview);
+        contactList.setAdapter(simpleCursorAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (contactsCursor != null) contactsCursor.close();
+    }
+
+    protected boolean hasContactPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) ==
+                PackageManager.PERMISSION_GRANTED;
+    }
+
+    void requestContactPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_CONTACTS},
+                0);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 0 && grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            loadContacts();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if (hasContactPermission()) {
+            loadContacts();
+        } else {
+            setContentView(R.layout.no_permissions);
+            requestContactPermission();
+        }
     }
 }
